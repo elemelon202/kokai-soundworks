7a4e6cbcaa6d48cd66b739a4aa16e858ed0c1d2d
kyle hansen <kylethansen17@gmail.com>
2025-11-25 14:23:20 +0900

    created the venue controller and methods

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,5 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :gigs, dependent: :destroy
+  has_many_attached :images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,15 +72,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_021729) do
   end
 
   create_table "kanban_tasks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "status"
-    t.string "task_type"
+    t.string "name", null: false
+    t.string "status", default: "to_do", null: false
+    t.bigint "created_by_id", null: false
+    t.string "task_type", null: false
     t.date "deadline"
     t.text "description"
-    t.integer "position"
-    t.integer "created_by_id"
+    t.integer "position", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_by_id"], name: "index_kanban_tasks_on_created_by_id"
+    t.index ["deadline"], name: "index_kanban_tasks_on_deadline"
+    t.index ["status"], name: "index_kanban_tasks_on_status"
+    t.index ["task_type"], name: "index_kanban_tasks_on_task_type"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -147,6 +151,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_021729) do
   add_foreign_key "gigs", "venues"
   add_foreign_key "involvements", "bands"
   add_foreign_key "involvements", "musicians"
+  add_foreign_key "kanban_tasks", "users", column: "created_by_id"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "musicians", "users"
