a68fdf9e21faec07563c7f798cf339805ac20dc1
Tyrhen Cameron <tyrhencameron@gmail.com>
2025-11-25 23:53:26 +0900

    error fix: changed user from has_many musicians to has_one musicians to fix error with pundit and band creation form

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,15 @@ class User < ApplicationRecord
   has_many :chats, through: :participations
   has_many :messages, dependent: :destroy
   has_many :venues, dependent: :destroy
-  has_many :musicians, dependent: :destroy
+  # has_many :musicians, dependent: :destroy #A user can only have one musician, itself right?
   has_many :gigs, through: :venues
   has_many :bookings, through: :bands
   has_many :message_reads
   has_many :read_messages, through: :message_reads, source: :message
+  # Tyrhen added this
+  has_one  :musician, dependent: :destroy
+
+  def musician?
+    user_type == 'musician'
+  end
 end
