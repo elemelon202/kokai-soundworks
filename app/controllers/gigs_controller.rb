e86ea9423d0f7580e4048d005aeb871ddfea38bf
kyle hansen <kylethansen17@gmail.com>
2025-12-01 10:05:10 +0900

    created conroller methods and updated model to include attendees

---
+++ app/controllers/gigs_controller.rb
@@ -0,0 +1,19 @@
+class GigsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,6 @@ class Gig < ApplicationRecord
   belongs_to :venue
   has_many :bookings, dependent: :destroy
   has_many :bands, through: :bookings
+  has_many :gig_attendances
+  has_many :attendees, through: :gig_attendances
 end
