ba760324cd0c2727c8e58546f9e190102882e9cb
kyle hansen <kylethansen17@gmail.com>
2025-12-03 14:39:15 +0900

    fixed end date to member migration

--- db/migrate/20251203030723_add_end_date_to_member_availabilities.rb
+++ db/migrate/20251203030723_add_end_date_to_member_availabilities.rb
@@ -1,6 +1,6 @@
 class AddEndDateToMemberAvailabilities < ActiveRecord::Migration[7.1]
   def change
-    rename_column :member_availabilities, :available_date, :start_date
+    rename_column :member_availabilities, :date, :start_date
     add_column :member_availabilities, :end_date, :date
   end
 end
